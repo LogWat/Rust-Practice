159f25a56028d96d9239398a5118e939ec56352e
LogWat <Nk8877wpt+Karan@gmail.com>
2021-08-16 15:46:04 +0900

    thread2

--- 3/thread.rs
+++ 3/thread.rs
@@ -1,7 +1,9 @@
 use std::thread;
+use std::rc::Rc;
 
 fn main() {
     let mut handles = Vec::new();
+    let mut handles2 = Vec::new();
 
     for x in 0..10 {
         handles.push(thread::spawn(move || {
@@ -12,4 +14,19 @@ fn main() {
     for handle in handles {
         let _ = handle.join();
     }
+
+    let mut data = Rc::new(Vec![1; 10]);
+
+    for x in 0..10 {
+        let data_ref = data.clone(); // <= Rcの参照カウンタ増加
+        handles2.push(thread::spawn(move || {
+            data_ref[x] += 1;
+        }));
+    }
+
+    for handle in handles2 {
+        let _ = handle.join();
+    }
+
+    dbg!(data);
 }
