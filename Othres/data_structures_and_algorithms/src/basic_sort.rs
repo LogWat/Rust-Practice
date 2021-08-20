066be57af4612d0ff9d261ccc6bdbed820ac7196
LogWat <Nk8877wpt+Karan@gmail.com>
2021-08-21 02:33:47 +0900

    selection sort

--- Othres/data_structures_and_algorithms/src/basic_sort.rs
+++ Othres/data_structures_and_algorithms/src/basic_sort.rs
@@ -1,13 +1,33 @@
-pub fn bubble_sort(array: &mut Vec<u8>) {
+// 基本交換法
+pub fn bubble_sort(array: &mut Vec<i32>) {
+
     let array_size = array.len();
 
     for _ in 0..array_size {
         for j in 0..array_size-1 {
             if array[j] > array[j+1] {
-                let tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
+                array.swap(j, j+1);
             }
+        }
+    }
+}
+
+// 基本選択法
+pub fn selection_sort(array: &mut Vec<i32>) {
+
+    let array_size = array.len();
+
+    for i in 0..array_size {
+        let mut min: i32 = 0;
+        let mut min_index = 0;
+        for j in i..array_size {
+            if j == i || min > array[j] {
+                min = array[j];
+                min_index = j;
+            }
+        }
+        if min_index != i {
+            array.swap(i, min_index);
         }
     }
 }
--- Othres/data_structures_and_algorithms/src/main.rs
+++ Othres/data_structures_and_algorithms/src/main.rs
@@ -3,18 +3,20 @@ pub mod basic_sort;
 
 // use crate::bin_search::bin_search_test;
 use crate::basic_sort::bubble_sort;
+use crate::basic_sort::selection_sort;
 use rand::Rng;
 
 fn main() {
     let n = 100;
-    let mut array: Vec<u8> = Vec::with_capacity(n); // 100個分の領域確保
+    let mut array: Vec<i32> = Vec::with_capacity(n); // 100個分の領域確保
     let mut rng = rand::thread_rng();      // randomのinit
 
     for _ in 0..n {
-        let i: u8 = rng.gen(); // generate
-        array.push(i);
+        let i: i32 = rng.gen(); // generate
+        array.push(i%255);
     }
-    bubble_sort(&mut array);
-    
+
+    // bubble_sort(&mut array);
+    selection_sort(&mut array);
     println!("{:?}", array);
 }
